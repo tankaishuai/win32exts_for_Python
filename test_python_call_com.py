d6315b57dea7b289b61bc9089212d9074a54c9bd
tankaishuai <tankaishuai@126.com>
2019-07-20 18:23:16 +0800

    add demo

---
+++ test_python_call_com.py
@@ -0,0 +1,12 @@
+﻿import win32exts
+
+#
+# 测试 COM 组件, 打开计算器
+#
+wsh = win32exts.co_create_object("Wscript.Shell")
+
+win32exts.co_push_start()
+win32exts.push_bstr("calc")
+win32exts.co_invoke(wsh, "Run")
+
+win32exts.co_release(wsh)
