b1885c1cc9feb4220540ae1c15b3681c2ed6f7ac
tankaishuai <41415395+tankaishuai@users.noreply.github.com>
2020-04-01 10:20:25 +0800

    Update test_python_call_com.py

--- test_python_call_com.py
+++ test_python_call_com.py
@@ -5,6 +5,9 @@
 #
 wsh = win32exts.co_create_object("Wscript.Shell")
 
+win32exts.va_invoke(wsh, "Run", "calc")
+
+#以上等价于以下三行
 win32exts.co_push_start()
 win32exts.push_bstr("calc")
 win32exts.co_invoke(wsh, "Run")
